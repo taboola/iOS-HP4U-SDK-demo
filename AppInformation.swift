421f0ce5e51733d33f2780fc599f46c2c2f435e9
Karen Shaham Palman <karen.s@taboola.com>
2022-05-25 19:48:48 +0300

    first commit

---
+++ AppInformation.swift
@@ -0,0 +1,8 @@
+//
+//  AppInformation.swift
+//  HomePageForYouDemoProject (iOS)
+//
+//  Created by Karen Shaham Palman on 25/05/2022.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# iOS-HP4U-SDK-demo
