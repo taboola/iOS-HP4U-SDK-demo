a811bf4f9b0a2267f1e5c11f5fcd4c27f1dfb1c4
Roman Slyepko <roman.slyepko@gmail.com>
2022-09-10 13:04:04 +0300

    MOB-1091 removed lazy/prefetch settings. removed all lazy load functionality

--- HomePageForYouDemoProject/Presentation/Settings/SettingsTableViewController.swift
+++ HomePageForYouDemoProject/Presentation/Settings/SettingsTableViewController.swift
@@ -12,20 +12,6 @@ class SettingsTableViewController: UITableViewController {
     @IBOutlet private weak var publisherTextfield: UITextField!
     @IBOutlet private weak var apiKeyTextfield: UITextField!
 
-    @IBOutlet private weak var preloadSwitch: UISwitch!
-    @IBOutlet private weak var lazyLoadSwitch: UISwitch!
-
-    @IBAction private func loadingSwitchStateDidChange(_ sender: UISwitch) {
-        let newState = sender.isOn
-        if sender == preloadSwitch {
-            preloadSwitch.isOn = newState
-            lazyLoadSwitch.isOn = !newState
-        } else if sender == lazyLoadSwitch {
-            lazyLoadSwitch.isOn = newState
-            preloadSwitch.isOn = !newState
-        }
-    }
-
     func setCredentials(publisher: String, apikey: String) {
         publisherTextfield.text = publisher
         apiKeyTextfield.text = apikey
@@ -35,9 +21,5 @@ class SettingsTableViewController: UITableViewController {
         guard let publisher = publisherTextfield.text, !publisher.isEmpty,
               let key = apiKeyTextfield.text, !key.isEmpty else { return nil }
         return (publisher, key)
-    }
-
-    func isPreloadSelected() -> Bool {
-        preloadSwitch.isOn
     }
 }
